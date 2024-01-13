caa4d96da034ba4b45ee9b789e48c1b62dfd565d
E-BlackTV <emirhancelik1133@icloud.com>
2024-01-13 12:56:08 +0100

    Pokemon Attributes ... added

--- src/at/wifi/codingacademy/PokemonGame/models/Player.java
+++ src/at/wifi/codingacademy/PokemonGame/models/Player.java
@@ -1,4 +1,5 @@
 package at.wifi.codingacademy.PokemonGame.models;
 
 public class Player {
+
 }
--- src/at/wifi/codingacademy/PokemonGame/models/Pokemon.java
+++ src/at/wifi/codingacademy/PokemonGame/models/Pokemon.java
@@ -1,4 +1,72 @@
 package at.wifi.codingacademy.PokemonGame.models;
 
 public class Pokemon {
+    protected String name;
+    protected Elements element;
+    protected int health;
+    protected int damage;
+    protected int defense;
+
+    //Konstruktor
+
+    public Pokemon(String name, Elements element, int health, int damage, int defense) {
+        this.name = name;
+        this.element = element;
+        this.health = health;
+        this.damage = damage;
+        this.defense = defense;
+    }
+
+    //Methoden
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Elements getElement() {
+        return element;
+    }
+
+    public void setElement(Elements element) {
+        this.element = element;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "name='" + name + '\'' +
+                ", element=" + element +
+                ", health=" + health +
+                ", damage=" + damage +
+                ", defense=" + defense +
+                '}';
+    }
 }
