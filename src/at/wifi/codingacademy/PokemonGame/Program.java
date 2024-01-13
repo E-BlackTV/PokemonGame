42f055cba250483f0689cc16de450ec286c8c2b1
E-BlackTV <emirhancelik1133@icloud.com>
2024-01-13 13:54:09 +0100

    Reformat

--- src/at/wifi/codingacademy/PokemonGame/Program.java
+++ src/at/wifi/codingacademy/PokemonGame/Program.java
@@ -1,5 +1,6 @@
 package at.wifi.codingacademy.PokemonGame;
 
+import at.wifi.codingacademy.PokemonGame.models.Attacks;
 import at.wifi.codingacademy.PokemonGame.models.Elements;
 import at.wifi.codingacademy.PokemonGame.models.Player;
 import at.wifi.codingacademy.PokemonGame.models.Pokemon;
@@ -7,14 +8,13 @@ import at.wifi.codingacademy.PokemonGame.models.Pokemon;
 import java.util.Random;
 
 public class Program {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-        Pokemon pikachu = new Pokemon("Pikachu", Elements.ELECTRICITY, 80, 130, 15, null );
+        Pokemon pikachu = new Pokemon("Pikachu", Elements.ELECTRICITY, 80, 130, 15, null);
         Pokemon glumanda = new Pokemon("Glumanda", Elements.FIRE, 90, 120, 20, null);
         Pokemon bisasam = new Pokemon("Bisasam", Elements.EARTH, 140, 55, 45, null);
         Pokemon flamiau = new Pokemon("Flamiau", Elements.FIRE, 100, 115, 10, null);
         Pokemon mewto = new Pokemon("Mewto", Elements.MAGIC, 500, 1000, 200, Attacks.INSTANTDEATH);
-
     }
 }
