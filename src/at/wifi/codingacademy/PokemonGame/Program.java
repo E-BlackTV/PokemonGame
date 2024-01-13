80437d7519f3e82e087d2410a30f07dfc5766044
E-BlackTV <emirhancelik1133@icloud.com>
2024-01-13 13:44:50 +0100

    Reformat

--- src/at/wifi/codingacademy/PokemonGame/Program.java
+++ src/at/wifi/codingacademy/PokemonGame/Program.java
@@ -1,43 +1,15 @@
 package at.wifi.codingacademy.PokemonGame;
-
-<<<<<<< HEAD
-import at.wifi.codingacademy.PokemonGame.models.Player;
-import at.wifi.codingacademy.PokemonGame.models.Pokemon;
-
-import java.util.Random;
-
-public class Program {
-    public static void main(String[] args){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    Player Ashe = new Player();
-    Ashe.addPokemon(new Pokemon());
-
-
-=======
 import at.wifi.codingacademy.PokemonGame.models.Attacks;
 import at.wifi.codingacademy.PokemonGame.models.Elements;
 import at.wifi.codingacademy.PokemonGame.models.Pokemon;
+import at.wifi.codingacademy.PokemonGame.models.Player;
+import java.util.Random;
 
 public class Program {
-    public static void main(String[] args){
-        Pokemon pikachu = new Pokemon("Pikachu", Elements.ELECTRICITY, 80, 130, 15, null );
+    public static void main(String[] args) {
+        Pokemon pikachu = new Pokemon("Pikachu", Elements.ELECTRICITY, 80, 130, 15, null);
         Pokemon glumanda = new Pokemon("Glumanda", Elements.FIRE, 90, 120, 20, null);
         Pokemon bisasam = new Pokemon("Bisasam", Elements.EARTH, 140, 55, 45, null);
         Pokemon flamiau = new Pokemon("Flamiau", Elements.FIRE, 100, 115, 10, null);
         Pokemon mewto = new Pokemon("Mewto", Elements.MAGIC, 500, 1000, 200, Attacks.INSTANTDEATH);
->>>>>>> f6c6caa735df5ff09161725a86558076007b1b26
     }
-}
