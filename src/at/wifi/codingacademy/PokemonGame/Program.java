723f8205cc0a75d96a65b1df2bc1eeeb2424b631
LAPTOP-G27QSL1A\Julian <bobyking3333@gmail.com>
2024-01-13 13:37:45 +0100

    Player, multiplier, Ashe player changes

--- src/at/wifi/codingacademy/PokemonGame/Program.java
+++ src/at/wifi/codingacademy/PokemonGame/Program.java
@@ -1,7 +1,29 @@
 package at.wifi.codingacademy.PokemonGame;
 
+import at.wifi.codingacademy.PokemonGame.models.Player;
+import at.wifi.codingacademy.PokemonGame.models.Pokemon;
+
+import java.util.Random;
+
 public class Program {
     public static void main(String[] args){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Player Ashe = new Player();
+    Ashe.addPokemon(new Pokemon());
+
 
     }
 }
--- src/at/wifi/codingacademy/PokemonGame/models/Player.java
+++ src/at/wifi/codingacademy/PokemonGame/models/Player.java
@@ -3,7 +3,52 @@ package at.wifi.codingacademy.PokemonGame.models;
 import java.util.ArrayList;
 
 public class Player {
-    protected String name;
     protected ArrayList<Pokemon> pokemons;
+    protected double attackMultiplier;
+    protected double healthMultiplier;
 
+    public Player(double attackMultiplier, double healthMultiplier) {
+        this.pokemons = new ArrayList<Pokemon>();
+        this.attackMultiplier = attackMultiplier;
+        this.healthMultiplier = healthMultiplier;
+    }
+
+    //Methods
+
+    public void addPokemon(Pokemon pokemon){
+        this.pokemons.add(pokemon);
+    }
+
+    public ArrayList<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    public void setPokemons(ArrayList<Pokemon> pokemons) {
+        this.pokemons = pokemons;
+    }
+
+    public double getAttackMultiplier() {
+        return attackMultiplier;
+    }
+
+    public void setAttackMultiplier(double attackMultiplier) {
+        this.attackMultiplier = attackMultiplier;
+    }
+
+    public double getHealthMultiplier() {
+        return healthMultiplier;
+    }
+
+    public void setHealthMultiplier(double healthMultiplier) {
+        this.healthMultiplier = healthMultiplier;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "pokemons: " + pokemons +
+                ", attackMultiplier: " + attackMultiplier +
+                ", healthMultiplier: " + healthMultiplier +
+                '}';
+    }
 }
