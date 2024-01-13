5a8ebd0cdd4aec8f39c1e36bb03d533c9d5f87b3
LAPTOP-G27QSL1A\Julian <bobyking3333@gmail.com>
2024-01-13 13:42:07 +0100

    Program merged

--- src/at/wifi/codingacademy/PokemonGame/Program.java
+++ src/at/wifi/codingacademy/PokemonGame/Program.java
@@ -1,6 +1,6 @@
 package at.wifi.codingacademy.PokemonGame;
 
-<<<<<<< HEAD
+import at.wifi.codingacademy.PokemonGame.models.Elements;
 import at.wifi.codingacademy.PokemonGame.models.Player;
 import at.wifi.codingacademy.PokemonGame.models.Pokemon;
 
@@ -8,36 +8,15 @@ import java.util.Random;
 
 public class Program {
     public static void main(String[] args){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     Player Ashe = new Player();
     Ashe.addPokemon(new Pokemon());
 
-
-=======
-import at.wifi.codingacademy.PokemonGame.models.Attacks;
-import at.wifi.codingacademy.PokemonGame.models.Elements;
-import at.wifi.codingacademy.PokemonGame.models.Pokemon;
-
-public class Program {
-    public static void main(String[] args){
         Pokemon pikachu = new Pokemon("Pikachu", Elements.ELECTRICITY, 80, 130, 15, null );
         Pokemon glumanda = new Pokemon("Glumanda", Elements.FIRE, 90, 120, 20, null);
         Pokemon bisasam = new Pokemon("Bisasam", Elements.EARTH, 140, 55, 45, null);
         Pokemon flamiau = new Pokemon("Flamiau", Elements.FIRE, 100, 115, 10, null);
         Pokemon mewto = new Pokemon("Mewto", Elements.MAGIC, 500, 1000, 200, Attacks.INSTANTDEATH);
->>>>>>> f6c6caa735df5ff09161725a86558076007b1b26
+
+        Ashe.addPokemon(new Pokemon());
     }
 }
